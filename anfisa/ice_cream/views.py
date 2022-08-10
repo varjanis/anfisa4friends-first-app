9e3c45fb4c727f535de4deaf92c1e86c7f9bd1c6
Alexandra Zvolinskaya <risa.infinita@gmail.com>
2022-08-10 16:10:08 +0300

    Added views.py

--- anfisa/ice_cream/views.py
+++ anfisa/ice_cream/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+# ice_cream/views.py
+from django.http import HttpResponse
+
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком мороженого
+def ice_cream_list(request):
+    return HttpResponse('Список мороженого')
+
+
+# Страница с информацией об одном сорте мороженого;
+# view-функция принимает параметр pk из path()
+def ice_cream_detail(request, pk):
+    return HttpResponse(f'Мороженое номер {pk}') 
